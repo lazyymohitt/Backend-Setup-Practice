11828c048577870778ee12d13a21e84569569de7
Mohit kumar <async.mohitt@gmail.com>
2025-03-29 18:43:12 +0530

    fix: improve MongoDB connection logging for better debugging

--- src/db/db.js
+++ src/db/db.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants";
 const connectDB = async ()=>{
  try {
     const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
-    console.log("")
+    console.log(`\n MonngoDB connected!!  DB Host :: ${connectionInstance} `)
  } catch (error) {
     console.log("MonogoDb connection ERROR",error)
     process.exit(1)
