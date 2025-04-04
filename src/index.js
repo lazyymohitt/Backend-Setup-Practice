b247576e9a7de06925db184c7fc596d970f523b5
Mohit kumar <async.mohitt@gmail.com>
2025-04-04 12:32:14 +0530

    feat: add cookies-parser and cors dependencies; initialize Express app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 dotenv.config({
-  path:'./env'
-})
+  path: "./env",
+});
 // import {DB_NAME} from "./constants.js";
 
-import mongoose from "mongoose"
-import express from "express"
+import mongoose from "mongoose";
+import express from "express";
 import connectDB from "./db/db.js";
 
-const app = express()
+const app = express();
 
 connectDB()
-
+  .then(() => {
+    app.on("Listener Error", (eventerror) => {
+      console.log("this error is seen in OnEvent In Then()", eventerror);
+      throw eventerror;
+    });
+    app.listen(process.env.PORT || 8000 ,()=>{
+      console.log( `the app is Running at ${process.env.PORT}`)
+    })
+  })
+  .catch((err) => {
+    console.log("This error is usully occured by the DB connection ", err);
+  });
