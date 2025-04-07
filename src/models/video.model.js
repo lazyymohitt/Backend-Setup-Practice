f6241554452ee4878a98891450c965155eb80581
Mohit kumar <async.mohitt@gmail.com>
2025-04-07 23:38:26 +0530

    feat: add mongoose-aggregate-paginate-v2 plugin to video model and update package dependencies

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,42 +1,45 @@
-import mongoose , {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const videoSchema  = new Schema({
-    videoFile:{
-        type:string , // get from Cloudinary
-        required:true
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: string, // get from Cloudinary
+      required: true,
     },
-    thumbnail:{
-        type:String,  // get form cloudinaryy
-        required:true    
+    thumbnail: {
+      type: String, // get form cloudinaryy
+      required: true,
     },
-    description:{
-        type:String,
-        required:true
+    description: {
+      type: String,
+      required: true,
     },
-    title:{
-        type:String,
-        required:true
+    title: {
+      type: String,
+      required: true,
     },
-    duration:{
-        type:Number, // you will get it form Cloudinary
-        required:true
+    duration: {
+      type: Number, // you will get it form Cloudinary
+      required: true,
     },
-    views:{
-        type:Number,
-        default:0
+    views: {
+      type: Number,
+      default: 0,
     },
-    isPublished:{
-        type:Boolean,
-        default:true
+    isPublished: {
+      type: Boolean,
+      default: true,
     },
-    owner:{
-        type: Schema.mongoose.Types.ObjectId,
-        ref:"User "
-    }    
+    owner: {
+      type: Schema.mongoose.Types.ObjectId,
+      ref: "User ",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-},{
-    timestamps:true
-})
-
-
-export  const Video  = mongoose.model("Video", videoSchema) 
+videoSchema.plugin(mongooseAggregatePaginate)
+export const Video = mongoose.model("Video", videoSchema);
