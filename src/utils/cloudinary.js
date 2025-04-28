95d19e21d0aa64ae604fbb7fc7f40a963360af16
Mohit kumar <async.mohitt@gmail.com>
2025-04-28 22:13:19 +0530

    feat: implement uploadOnCloudinary function and multer middleware for file uploads

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -7,3 +7,25 @@ cloudinary.config({
     api_key:process.env.API_KEY,
     api_secret:process.env.API_SECRET
 })
+
+const uploadOnCloudinary = async(localFilePath)=>{
+        try {
+            if(!localFilePath) return null
+
+            // now start uploading file 
+           const response  = await  cloudinary.uploader.upload(localFilePath,{
+                resource_type:"auto"
+             })
+            // file has been succesfully uploaded 
+
+            console.log("file has been Uploaded",response.url);
+            return response;
+        } catch (error) {
+            fs.unlinkSync(localFilePath)// remove locally save  te,porary file as the upload operation got failed
+            return null
+            
+        }
+}
+
+
+export {uploadOnCloudinary}
